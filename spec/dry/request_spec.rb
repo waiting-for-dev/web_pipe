e5c2b98ff0939a484c32f86a365bed583dfe6184
Marc Busqué <marc@lamarciana.com>
2019-02-22 19:19:23 +0100

    Remove dummy test

--- spec/dry/request_spec.rb
+++ spec/dry/request_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Dry::Request do
   it "has a version number" do
     expect(Dry::Request::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
