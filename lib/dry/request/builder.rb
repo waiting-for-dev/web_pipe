5f73acd29abd17d11e5700448c32a3e7839dfe9c
Marc Busqué <marc@lamarciana.com>
2019-02-28 12:10:35 +0100

    Simplify instance methods definition

--- lib/dry/request/builder.rb
+++ lib/dry/request/builder.rb
@@ -8,27 +8,45 @@ module Dry
       EMPTY_CONTAINER = {}
 
       attr_reader :class_context
-      attr_reader :container
 
       def initialize(container: EMPTY_CONTAINER)
-        @class_context = ClassContext.new
-        @container = container
+        @class_context = ClassContext.new(container: container)
       end
 
       def included(klass)
         klass.extend(class_context)
-        klass.include(InstanceContext.new(class_context, container))
+        klass.include(InstanceMethods)
       end
 
       class ClassContext < Module
         attr_reader :steps
+        attr_reader :container
 
-        def initialize
+        def initialize(container:)
           @steps = []
+          @container = container
+          define_steps
+          define_container
           define_plug_method
         end
 
         private
+
+        def define_steps
+          module_exec(steps) do |steps|
+            define_method(:steps) do
+              steps
+            end
+          end
+        end
+
+        def define_container
+          module_exec(container) do |container|
+            define_method(:container) do
+              container
+            end
+          end
+        end
 
         def define_plug_method
           module_exec(steps) do |steps|
@@ -39,63 +57,42 @@ module Dry
         end
       end
 
-      class InstanceContext < Module
+      module InstanceMethods
         attr_reader :steps
         attr_reader :container
 
-        def initialize(class_context, container)
-          @steps = class_context.steps
-          @container = container
+        include Dry::Monads::Result::Mixin
+
+        def initialize(**kwargs)
+          @steps = self.class.steps.map do |(name, op)|
+            kwargs.has_key?(name) ? [name, kwargs[name]] : [name, op]
+          end
+          @container = self.class.container
         end
 
-        def included(klass)
-          klass.attr_reader :steps
-          klass.attr_reader :container
-          klass.include Dry::Monads::Result::Mixin
-          define_initialize_method
-          define_call_method
-        end
+        def call(env)
+          conn = Success(CleanConn.new(env))
+          resolver = Resolver.new(container)
 
-        private
-
-        def define_initialize_method
-          module_exec(steps, container) do |steps, container|
-            define_method :initialize do |**kwargs|
-              @steps = steps.map do |(name, op)|
-                kwargs.has_key?(name) ? [name, kwargs[name]] : [name, op]
+          last_conn = steps.reduce(conn) do |prev_conn, (_name, step)|
+            prev_conn.bind do |c|
+              result = resolver.(step).(c)
+              case result
+              when CleanConn
+                Success(result)
+              when DirtyConn
+                Failure(result)
+              else
+                raise RuntimeError
               end
-              @container = container
             end
           end
-        end
 
-        def define_call_method
-          module_exec do
-            define_method :call do |env|
-              conn = Success(CleanConn.new(env))
-              resolver = Resolver.new(container)
-
-              last_conn = steps.reduce(conn) do |prev_conn, (_name, step)|
-                prev_conn.bind do |c|
-                  result = resolver.(step).(c)
-                  case result
-                  when CleanConn
-                    Success(result)
-                  when DirtyConn
-                    Failure(result)
-                  else
-                    raise RuntimeError
-                  end
-                end
-              end
-
-              case last_conn
-              when Dry::Monads::Success
-                last_conn.success.rack_response
-              when Dry::Monads::Failure
-                last_conn.failure.rack_response
-              end
-            end
+          case last_conn
+          when Dry::Monads::Success
+            last_conn.success.rack_response
+          when Dry::Monads::Failure
+            last_conn.failure.rack_response
           end
         end
       end
