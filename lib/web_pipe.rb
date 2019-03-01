1d8344ba974cd63f4a66ac8a280f33ab8004d779
Marc Busqué <marc@lamarciana.com>
2019-03-01 19:35:31 +0100

    Fix docs

--- lib/web_pipe.rb
+++ lib/web_pipe.rb
@@ -2,7 +2,7 @@ require 'web_pipe/builder'
 
 module WebPipe
   # When this module is included, `WebPipe.included` just delegates to an
-  # instance of `Builder`, so `Builder#instance` is finally called.
+  # instance of `Builder`, so `Builder#included` is finally called.
   def self.included(klass)
     klass.include(WebPipe::call())
   end
