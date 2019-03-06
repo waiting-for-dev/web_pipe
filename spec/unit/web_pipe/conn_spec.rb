5753bf5c4e26aa77f90120c474944b5ca7fcee64
Marc Busqué <marc@lamarciana.com>
2019-03-06 19:12:37 +0100

    Test empty query string gives empty params hash

--- spec/unit/web_pipe/conn_spec.rb
+++ spec/unit/web_pipe/conn_spec.rb
@@ -5,12 +5,22 @@ require 'support/env'
 RSpec.describe WebPipe::Conn do
   subject(:conn) { described_class.build(env) }
 
-  describe "#request" do
-    describe "#params" do
-      let(:env) { DEFAULT_ENV.merge("QUERY_STRING" => "foo=bar") }
+  describe '#request' do
+    describe '#params' do
+      context 'when there is query string' do
+        let(:env) { DEFAULT_ENV.merge("QUERY_STRING" => "foo=bar") }
 
-      it "returns hash with query parameters" do
-        expect(conn.request.params).to eq({ "foo" => "bar" })
+        it 'returns hash with query parameters' do
+          expect(conn.request.params).to eq({ "foo" => "bar" })
+        end
+      end
+
+      context 'when there is no query string' do
+        let(:env) { DEFAULT_ENV.merge("QUERY_STRING" => "") }
+
+        it 'returns the empty hash' do
+          expect(conn.request.params).to eq({})
+        end
       end
     end
   end
