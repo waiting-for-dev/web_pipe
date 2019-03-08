ebbc27be1436db97ba6ad170ec894f41899a0b00
Marc Busqué <marc@lamarciana.com>
2019-03-08 19:06:15 +0100

    Refactor conn build tests

--- spec/unit/web_pipe/conn/builder_spec.rb
+++ spec/unit/web_pipe/conn/builder_spec.rb
@@ -5,30 +5,30 @@ require 'support/env'
 
 RSpec.describe WebPipe::Conn::Builder do
   describe ".call" do
-    subject(:conn) { described_class.call(env) }
+    it 'creates a CleanConn' do
+      conn = described_class.call(DEFAULT_ENV)
 
-    context 'creation' do
-      let(:env) { DEFAULT_ENV }
-
-      it 'creates a CleanConn' do
-        expect(conn).to be_an_instance_of(WebPipe::CleanConn)
-      end
+      expect(conn).to be_an_instance_of(WebPipe::CleanConn)
     end
 
     context 'request' do
       context 'params' do
         context 'when there is query string' do
-          let(:env) { DEFAULT_ENV.merge("QUERY_STRING" => "foo=bar") }
-
           it 'fills with query parameters' do
+            env = DEFAULT_ENV.merge("QUERY_STRING" => "foo=bar")
+
+            conn = described_class.call(env)
+
             expect(conn.request.params).to eq({ "foo" => "bar" })
           end
         end
 
         context 'when there is no query string' do
-          let(:env) { DEFAULT_ENV.merge("QUERY_STRING" => "") }
-
           it 'fills with empty hash' do
+            env = DEFAULT_ENV.merge("QUERY_STRING" => "")
+
+            conn = described_class.call(env)
+
             expect(conn.request.params).to eq({})
           end
         end
@@ -36,17 +36,19 @@ RSpec.describe WebPipe::Conn::Builder do
 
       context 'headers' do
         context 'when there is some request headers' do
-          let(:env) { DEFAULT_ENV.merge("HTTP_FOO" => "BAR") }
-
           it 'fills them as a hash' do
+            env = DEFAULT_ENV.merge("HTTP_FOO" => "BAR")
+
+            conn = described_class.call(env)
+
             expect(conn.request.headers).to eq({ "FOO" => "BAR" })
           end
         end
 
         context 'when there is no request headers' do
-          let(:env) { DEFAULT_ENV }
-
           it 'fills with empty hash' do
+            conn = described_class.call(DEFAULT_ENV)
+
             expect(conn.request.headers).to eq({})
           end
         end
