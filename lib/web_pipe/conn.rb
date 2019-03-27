a750224b0ead4aa76dd21236537b1685f35cc0c6
Marc Busqué <marc@lamarciana.com>
2019-03-27 11:19:40 +0100

    Add comments to make clearer source of information

--- lib/web_pipe/conn.rb
+++ lib/web_pipe/conn.rb
@@ -7,19 +7,22 @@ module WebPipe
     attr_accessor :resp_body
 
     attribute :request do
+      # Rack
       attribute :rack_env, Types::Request::RackEnv
       attribute :rack_request, Types::Request::RackRequest
-      attribute :params, Types::Request::Params
-      attribute :headers, Types::Request::Headers
+      # Request
+      attribute :scheme, Types::Request::Scheme
       attribute :req_method, Types::Request::Method
-      attribute :script_name, Types::Request::ScriptName
       attribute :host, Types::Request::Host
+      attribute :ip, Types::Request::Ip
+      attribute :port, Types::Request::Port
+      attribute :script_name, Types::Request::ScriptName
       attribute :path_info, Types::Request::PathInfo
       attribute :query_string, Types::Request::QueryString
-      attribute :port, Types::Request::Port
-      attribute :ip, Types::Request::Ip
+      attribute :headers, Types::Request::Headers
+      # Redundant
       attribute :base_url, Types::Request::BaseUrl
-      attribute :scheme, Types::Request::Scheme
+      attribute :params, Types::Request::Params
 
       def fetch_params
         Builder.call(rack_env).new(request: new(params: rack_request.params))
