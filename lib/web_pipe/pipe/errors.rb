b26bd77577f4d86a4a7b8d8704dc63366df92889
Marc Busqué <marc@lamarciana.com>
2019-04-24 18:59:35 +0200

    Document and refactor errors module

--- lib/web_pipe/pipe/errors.rb
+++ lib/web_pipe/pipe/errors.rb
@@ -1,8 +1,16 @@
 module WebPipe
   module Pipe
+    # Error raised when no operation can be built through the DSL for
+    # building `Plug`.
     class InvalidPlugError < ArgumentError
       def initialize(name)
-        super("Plug with name #{name} is invalid. It must be something callable, an instance method when `with:` is not given, or something callable registered in the container.")
+        super(
+          <<~eos
+            Plug with name #{name} is invalid. It must be something
+            callable, an instance method when `with:` is not given, or
+            something callable registered in the container."
+          eos
+        )
       end
     end
   end
