2acb9115f53ad393f3452d3c6d6b9e4b5bd08787
Marc Busqué <marc@lamarciana.com>
2019-07-25 08:11:30 +0200

    Add extension adding flash bag functionality
    ```ruby
    require 'web_pipe'
    require 'rack/session/cookie'
    require 'rack-flash'
    
    WebPipe.load_extensions(:flash)
    
    class MyApp
      include WebPipe
    
      use :session, Rack::Session::Cookie, secret: 'secret'
      use :flash, Rack::Flash
    
      plug :put_in_flash, ->(conn) { conn.put_flash(:notice, 'Hello world') }
      plug :put_in_flash_now, ->(conn) { conn.put_flash_now(:notice_now, 'Hello world now') }
    end
    ```
    
    For this extension to be used, `Rack::Flash` middleware must be added to
    the stack (gem name is `rack-flash3`. This middleware in turns depend on
    `Rack::Session` middleware.

---
+++ lib/web_pipe/extensions/flash/flash.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'web_pipe/conn'
+require 'web_pipe/conn_support/errors'
+
+module WebPipe
+  # Provides with a tipical flash messages functionality.
+  #
+  # @example
+  #   require 'web_pipe'
+  #   require 'rack/session/cookie'
+  #   require 'rack-flash'
+  #
+  #   WebPipe.load_extensions(:flash)
+  #   
+  #   class MyApp
+  #     include WebPipe
+  #
+  #     use :session, Rack::Session::Cookie, secret: 'secret'
+  #     use :flash, Rack::Flash
+  #
+  #     plug :put_in_flash, ->(conn) { conn.put_flash(:notice, 'Hello world') }
+  #     plug :put_in_flash_now, ->(conn) { conn.put_flash_now(:notice_now, 'Hello world now') }
+  #   end
+  #
+  # Usually, you will end up making `conn.flash` available to your view system:
+  #
+  # @example
+  #   <div class="notice"><%= flash[:notice] %></div>
+  #
+  # For this extension to be used, `Rack::Flash` middleware must be
+  # added to the stack (gem name is `rack-flash3`. This middleware in
+  # turns depend on `Rack::Session` middleware.
+  #
+  # This extension is a very simple wrapper around `Rack::Flash` API.
+  #
+  # @see https://github.com/nakajima/rack-flash
+  class Conn < Dry::Struct
+    RACK_FLASH_KEY = 'x-rack.flash'
+    
+    # Returns the flash bag.
+    #
+    # @return [Rack::Flash::FlashHash]
+    #
+    # @raises ConnSupport::MissingMiddlewareError when `Rack::Flash`
+    # is not being used as middleware
+    def flash
+      env.fetch(RACK_FLASH_KEY) do
+        raise ConnSupport::MissingMiddlewareError.new(
+                'flash', 'Rack::Flash', 'rack-flash3'
+              )
+      end
+    end
+
+    # Puts an item to the flash bag to be consumed by next request.
+    #
+    # @param key [String]
+    # @param value [String]
+    def put_flash(key, value)
+      flash[key] = value
+      self
+    end
+
+    # Puts an item to the flash bag to be consumed by the same request
+    # in process.
+    #
+    # @param key [String]
+    # @param value [String]
+    def put_flash_now(key, value)
+      flash.now[key] = value
+      self
+    end
+  end
+end
---
+++ spec/extensions/flash/flash_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'support/env'
+require 'web_pipe/conn'
+require 'web_pipe/conn_support/builder'
+require 'web_pipe/conn_support/errors'
+require 'rack-flash'
+
+RSpec.describe WebPipe::Conn do
+  before do
+    WebPipe.load_extensions(:flash)
+  end
+
+  let(:flash) { Rack::Flash::FlashHash.new({}) }
+
+  describe '#flash' do
+    context "when rack-flash key is found in env" do
+      it 'returns its value' do
+        env = DEFAULT_ENV.merge('x-rack.flash' => flash)
+        conn = WebPipe::ConnSupport::Builder.call(env)
+
+        expect(conn.flash).to be(flash)
+      end
+    end
+
+    context "when rack-flash key is not found in env" do
+      it 'raises a MissingMiddlewareError' do
+        conn = WebPipe::ConnSupport::Builder.call(DEFAULT_ENV)
+
+        expect { conn.flash }.to raise_error(WebPipe::ConnSupport::MissingMiddlewareError)
+      end
+    end
+  end
+
+  describe '#put_flash' do
+    it 'sets given key to given value in flash' do
+      env = DEFAULT_ENV.merge('x-rack.flash' => flash)
+      conn = WebPipe::ConnSupport::Builder.call(env)
+
+      conn = conn.put_flash(:error, 'error')
+
+      expect(flash[:error]).to eq('error')
+    end
+  end
+
+  describe '#put_flash_now' do
+    it 'sets given key to given value in flash cache' do
+      env = DEFAULT_ENV.merge('x-rack.flash' => flash)
+      conn = WebPipe::ConnSupport::Builder.call(env)
+
+      conn = conn.put_flash_now(:error, 'error')
+
+      expect(flash.send(:cache)[:error]).to eq('error')
+    end
+  end
+end
