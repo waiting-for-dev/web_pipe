1ed9c5a92d4cd7cfa5f97ff9fb6df2bc011e60ea
Marc Busqué <marc@lamarciana.com>
2019-07-29 07:31:25 +0200

    Add extension to easily create redirects
    If none is given, default status code is 302.
    
    ```ruby
    require 'web_pipe'
    
    WebPipe.load_extensions(:redirect)
    
    class MyApp
      include WebPipe
    
      plug(:redirect) do |conn|
        conn.redirect('/') # same as conn.redirect('/', 302)
      end
    end
    ```

---
+++ spec/extensions/redirect/redirect_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'support/env'
+require 'web_pipe/conn_support/builder'
+
+RSpec.describe WebPipe::Conn do
+  before { WebPipe.load_extensions(:redirect) }
+
+  describe '#redirect' do
+    let(:conn) { WebPipe::ConnSupport::Builder.(DEFAULT_ENV) }
+    let(:new_conn) { conn.redirect('/here') }
+
+    it 'uses 302 as default status code' do
+      expect(new_conn.status).to be(302)
+    end
+
+    it 'sets given path as Location header' do
+      expect(new_conn.response_headers['Location']).to eq('/here')
+    end
+  end
+end
